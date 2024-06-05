4a9692df0a6535ca00a2e3a0e6c722423bd86d8a
servend <147636657+servend@users.noreply.github.com>
2024-06-05 17:56:55 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿using alfa;
+using alfa;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,13 +12,20 @@ class Program
     {
         List<Organization> organizations = new List<Organization>();
         Excel.Excelobj(organizations);
+        await Httprequest.Request(organizations);
+        Excel.INN_folder_create();
+        Excel.INN_file_create();
+        List<Organization> Filtredorganizations = new List<Organization>();
+        filtrorg(organizations, Filtredorganizations);
+        Excel.INN_file_addobj(Filtredorganizations);
+       
+    }
+    public static void filtrorg(List<Organization> organizations, List<Organization> Filtredorganizations)
+    {
         foreach (Organization organization in organizations)
         {
-            Console.WriteLine(1);
-            await Httprequest.Request(organization);
-        }
+            if (organization.Dubl != "Да") {Filtredorganizations.Add(organization);}
 
-        // Выполняем HTTP-запрос для каждой организации
-       
+        }
     }
 }
