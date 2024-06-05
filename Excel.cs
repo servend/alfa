26635d9fbf57f9fbb2cde6c1a319a297e01d9165
servend <147636657+servend@users.noreply.github.com>
2024-06-05 17:56:38 +0300

    Update Excel.cs

--- Excel.cs
+++ Excel.cs
@@ -1,4 +1,4 @@
-﻿using OfficeOpenXml;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -38,8 +38,9 @@ namespace alfa
                         // Получаем значение номера телефона
                         string number = worksheet.Cells[row, 2].Value.ToString();
                         number = Regex.Replace(number, "[^0-9]", "");
+                        string name = worksheet.Cells[row, 3].Value.ToString();
                         // Создаем объект Organization и добавляем его в список
-                        Organization organization = new Organization(inn, number);
+                        Organization organization = new Organization(inn, number," ",name);
                         organizations.Add(organization);
                     }
                 }
@@ -47,6 +48,54 @@ namespace alfa
 
             // Выводим список организаций
             
+        }
+        public static void INN_folder_create()
+        {
+            string folderPath = @"C:\INN_Output";
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+                Console.WriteLine("Папка INN_Output успешно создана.");
+            }
+            else
+            {
+                Console.WriteLine("Папка INN_Output уже существует на диске C.");
+            }
+        }
+        public static void INN_file_create()
+        {
+            string folderPath = @"C:\INN_Output\ИНН.xlsx";
+
+            if (!Directory.Exists(folderPath))
+            {
+                ExcelPackage excel = new ExcelPackage();
+                ExcelWorksheet worksheet = excel.Workbook.Worksheets.Add("ИНН");
+                Console.WriteLine("Файл ИНН успешно создан.");
+                FileInfo excelFile = new FileInfo(folderPath);
+                excel.SaveAs(excelFile);
+            }
+            else
+            {
+                Console.WriteLine("Файл ИНН уже существует на диске C.");
+            }
+        }
+        public static void INN_file_addobj(List<Organization> organizations)
+        {
+            string folderPath = @"C:\INN_Output\ИНН.xlsx";
+            using (ExcelPackage excelPackage = new ExcelPackage(new FileInfo(folderPath)))
+            {
+                ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets[0];
+                foreach(Organization organization in organizations)
+                {
+                    worksheet.Cells["A1"].LoadFromCollection(organizations.Select(obj => new { ((dynamic)obj).Name, ((dynamic)obj).Number, ((dynamic)obj).INN }), true);
+                    worksheet.Cells["A1"].Value = "Организация";
+                    worksheet.Cells["B1"].Value = "Телефон";
+                    worksheet.Cells["C1"].Value = "ИНН";
+                    FileInfo excelFile = new FileInfo(folderPath);
+                    excelPackage.SaveAs(excelFile);
+                }
+            }
         }
     }
     
